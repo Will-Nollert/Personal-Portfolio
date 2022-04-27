1488426e1bdff07bc0bffd3ebf50bdc02772f115
William Nollert <will.nollert@gmail.com>
2022-04-27 11:33:26 -0500

    backend setup with sanity

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,30 +1,30 @@
 export default {
-  name: "testimonals",
-  title: "Testimonials",
-  type: "document",
-  fields: [
-    {
-      name: "name",
-      titel: "Name",
-      type: "string",
-    },
-    {
-      name: "company",
-      titel: "Company",
-      type: "string",
-    },
-    {
-      name: "imageurl",
-      titel: "Imgurl",
-      type: "image",
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: "feedback",
-      titel: "Feedback",
-      type: "string",
-    },
-  ],
-};
+    name:'testimonials',
+    title:'Testimonials',
+    type: 'document',
+    fields:[
+        { 
+            name:'name',
+            title:'Name',
+            type: 'string'
+        },
+        {
+            name:'company',
+            title:'Company',
+            type:'string'
+        },
+        {
+            name:'imgurl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },  
+        {
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
+        }
+    ]
+}
